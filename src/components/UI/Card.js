af55c26bb8f0710c8560f0a76496e20e34635e86
puja.rani <puja.rani@ril.com>
2022-02-28 10:35:23 +0530

    feat: Create Meal Item component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import classes from "./Card.module.css";
+
+const Card = (props) => {
+  const classList = classes.card + ` ${props.className}`;
+  return <div className={classList}>{props.children}</div>;
+};
+
+export default Card;
