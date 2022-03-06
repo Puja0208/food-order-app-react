58c3acd30aaf64e35ec4f1a2bdb912e13d1a0665
puja.rani <puja.rani@ril.com>
2022-03-06 06:31:50 +0530

    feat: Create checkout form component

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,26 @@
+import classes from "./Checkout.module.css";
+const Checkout = () => {
+  return (
+    <form>
+      <div className={classes.control}>
+        <label htmlFor="name">Your name</label>
+        <input id="name" type="text" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input id="street" type="text" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input id="postal" type="text" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input id="city" type="text" />
+      </div>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Checkout;
